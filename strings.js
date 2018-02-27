aa2c4a42b623078d1bd3e83f420e9c5cbe980a39
Amanda Slayton <alslayton86@gmail.com>
2018-02-26 20:41:41 -0600

    challenge 3

--- strings.js
+++ strings.js
@@ -9,3 +9,9 @@ var name = "Amanda";
 var string = ". How are you doing?"
 console.log("Hello, " + name + string);
 
+//Challenge #3
+var DNA = "GCAT"
+var RNA = DNA.replace(/T/g,'U');
+
+console.log("RNA:", RNA); //SHOULD PRINT OUT "GCAU"
+

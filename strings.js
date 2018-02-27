6fd3e3f60793aae53d58a8284f4d1903c0ffdc22
Amanda Slayton <alslayton86@gmail.com>
2018-02-26 20:30:44 -0600

    second challenge

--- strings.js
+++ strings.js
@@ -3,3 +3,9 @@ var friends = 4;
 var sandwiches = 2;
 var numOfSandwiches = friends * sandwiches;
 console.log("numOfSandwiches", numOfSandwiches);
+
+//Challenge #2
+var name = "Amanda";
+var string = ". How are you doing?"
+console.log("Hello, " + name + string);
+
